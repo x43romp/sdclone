e31ff4d89e3ab0fd3bd2586551176d24c29696b8
John Untivero <jpuntivero@gmail.com>
2019-09-03 20:22:01 -0500

    commander started
    started the commander CLI program

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-process.stdout.write("Hello World");
+#!/usr/bin/env node
+import program = require('commander');
+
+program
+    .version('0.0.1')
+    .description('Shadow Clone -- Automated file checksum');
+
+
+program.parse(process.argv)
