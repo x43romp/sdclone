4ab7db82620f7dba531422e88ad47cebcc6f5edf
John Untivero <jpuntivero@gmail.com>
2019-10-02 15:13:54 -0500

    Archive.constructor - updated constructor
    removed the trycatch and replaced with throw new Error

--- src/sdArchive.ts
+++ src/sdArchive.ts
@@ -23,20 +23,17 @@ export class Archive implements ArchiveInterface {
 
     constructor(path: string, options?: string) {
         // cleanup path
-        try {
-            this.directory = directPath(path);
-            if (!expectDir(this.directory)) {
-                this.directory = null;
-                throw "invalid archivepath";
-            }
-            
-            // TODO: create a code that scans for mhl or md5 files in the directory
-
-            let fileList: string[] = scandir(this.directory);
-            this.files = fileList.map(file => { return new Hash(file, this.directory); });
-        } catch (error) {
-            console.error(error);
+        this.directory = directPath(path);
+        if (!expectDir(this.directory)) {
+            this.directory = null;
+            throw new Error("invalid path - must be a real ")
         }
+
+        // TODO: create a code that scans for mhl or md5 files in the directory
+
+        let fileList: string[] = scandir(this.directory);
+        this.files = fileList.map(file => { return new Hash(file, this.directory); });
+
     }
 
     public stream() {
