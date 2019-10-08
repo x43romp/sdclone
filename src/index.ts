27250326bd59499ab7eb413f4211311a8073d4db
John Untivero <jpuntivero@gmail.com>
2019-10-08 17:00:14 -0500

    cli seal command updated

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import { join, parse } from 'path';
 import { expectFile, directPath, scandir, printProgressOptions, printProgress, expectDir } from './sdTools';
-import { Hash, HashProgress } from './sdHash';
+import { Hash, HashProgress, HashInterface } from './sdHash';
 
 import program = require('commander');
 import readline = require('readline');
@@ -11,75 +11,43 @@ program
     .version('0.0.1')
     .description('Shadow Clone -- Automated file checksum');
 
-program
-    .command('test <path>')
-    .action((path, options) => {
-        let a = new Archive(path);
-        // console.log(a.getFiles());
-        a.stream();
-    });
-
 // Seal command
 program
     .command('seal <path>')
     .description('seals a directory')
     .action(async (path, options) => {
-        try {
-            // Fill unassigned options will defaults
-            let optionsDefs = { interval: 500 };
-            options = Object.assign(optionsDefs, options);
+        let archive = new Archive(path);
+        let progress: printProgressOptions = {
+            index: 0, indexTotal: archive.getFiles().length,
+            progressTotal: 0, progressLength: 32,
+            progressDone: null
+        };
 
-            // fix path
-            path = directPath(path);
-            console.log(`=====\nSealing [ ${path} ]\n=====`);
+        console.log(`=====\nSealing [ ${path} ]\n=====`);
+        archive.stream();
+        let timer = new Date().getTime();
+        archive.on('data', (data: HashProgress, counter) => {
+            if (new Date().getTime() < timer)
+                return;
 
-            // record time start
-            let time = process.hrtime();
+            readline.clearLine(process.stdout, 0);
+            readline.cursorTo(process.stdout, 0, null);
+            progress.index = counter;
+            progress.progress = data.sizeread;
+            progress.progressTotal = data.filesize;
+            progress.progressDone = null;
+            process.stdout.write(printProgress(data.filename, progress));
 
-            // get list of files
-            let files: string[] = [];
-            if (expectDir(path)) files = scandir(path);
-            else if (expectFile(path)) { files = [parse(path).base]; path = parse(path).dir; }
-            else throw `not a valid path: ${path}`;
-
-            let filecounter = 0;
-            for (let file of files) {
-                filecounter++;
-
-                // get the full filepath and initalize Hash
-                let filepath = join(path, file);
-                let hash = new Hash(filepath);
-
-                // configure printProgress options
-                const progressOpts: printProgressOptions = {
-                    index: filecounter, indexTotal: files.length,
-                    progressTotal: hash.filesize, progressLength: 32,
-                    progressDone: null,
-                }
-
-                await hash.stream({ interval: options.interval }, (data: HashProgress, error) => {
-                    readline.clearLine(process.stdout, 0);
-                    readline.cursorTo(process.stdout, 0, null);
-                    progressOpts.progress = data.sizeread;
-                    process.stdout.write(printProgress(file, progressOpts));
-                }).then((data) => {
-                    readline.clearLine(process.stdout, 0);
-                    readline.cursorTo(process.stdout, 0);
-                    progressOpts.progressDone = data.hash;
-                    console.log(printProgress(file, progressOpts));
-                });
-            }
-
-            time = process.hrtime(time);
-
-            // console.log(`This process took ${process.hrtime(time)[0]} seconds`);
-            console.log(`=====\nThis process took ${(time[0] + time[1] / 1e9).toFixed(3)} seconds\n=====`);
-
-
-        } catch (error) {
-            console.error(error);
-        }
-
+            timer = new Date().getTime() + 500;
+        }).on('hash', (data: HashInterface, counter) => {
+            readline.clearLine(process.stdout, 0);
+            readline.cursorTo(process.stdout, 0);
+            progress.index = counter;
+            progress.progressDone = data.hash;
+            console.log(printProgress(data.filename, progress));
+        }).on('done', () => {
+            let totalbytes = archive.files.reduce((acc, file) => acc + file.filesize, 0);
+        });
     });
 
 program.parse(process.argv)
