4953fb06a6f6c151fbb9943e23166dd4fb3b14c6
John Untivero <jpuntivero@gmail.com>
2020-03-31 20:26:43 -0500

    format/sha - uses crypto.createHash

--- src/formats/sha.ts
+++ src/formats/sha.ts
@@ -1,24 +1,28 @@
 import HashTemplate from './_template'
 import { directPath } from '../hash'
-import { statSync } from 'fs'
-import { exec } from 'child_process'
+import { statSync, createReadStream } from 'fs'
+import { createHash } from 'crypto'
 
 export default class HashSHA extends HashTemplate {
-  EXTENSIONS = ['sha1', 'sha'];
+  EXTENSIONS = ['sha1', 'sha']
 
   public async hash(filepath: string): Promise<string> {
     const path = directPath(filepath)
     if (statSync(path).isFile() === false) throw new Error('Not a valid file')
 
-    const prepared = path.replace(' ', '\\ ')
     return new Promise((resolve, reject) => {
-      exec(`sha1sum ${prepared}`, (error, stdout, stderr) => {
-        if (error) reject(error)
-        if (stderr) reject(stderr.toString())
+      try {
+        const hash = createHash('sha1').setEncoding('hex')
+        const read = createReadStream(path)
 
-        const split = stdout.split(' ')[0]
-        resolve(split)
-      })
+        read.pipe(hash)
+
+        read.on('end', () => {
+          resolve(hash.read())
+        })
+      } catch (error) {
+        reject(error)
+      }
     })
   }
 
