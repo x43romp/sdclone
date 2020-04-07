bba89e5e4ad5e4ef3a4720ecdd15eb1d4ec32784
John Untivero <jpuntivero@gmail.com>
2020-04-06 19:42:07 -0500

    format mhl - added seal

--- src/formats/mhl.ts
+++ src/formats/mhl.ts
@@ -1,6 +1,8 @@
-import HashTemplate from './_template';
-import { HashData, HashVerifyOptions } from '../hash';
-import HashMD5 from './md5';
+import HashTemplate from './_template'
+import { HashData, HashVerifyOptions } from '../hash'
+import HashMD5 from './md5'
+import * as OS from 'os'
+import { js2xml } from 'xml-js'
 
 interface Mhl {
   hashlist_version: string;
@@ -34,7 +36,58 @@ export default class HashMHL extends HashTemplate {
   }
 
   public seal(data: HashData[] | HashData): string {
-    return super.seal(data)
+    data = (Array.isArray(data)) ? data : [data]
+
+    const creator: MhlCreator = {
+      name: OS.userInfo().username,
+      username: OS.userInfo().username,
+      hostname: OS.hostname(),
+      tool: `sdclone ${process.version.toString()}`,
+      startdate: new Date().toISOString(),
+      finishdate: new Date().toISOString(),
+    }
+
+    const lines: MhlHash[] = data.map(line => {
+      const resp: MhlHash = {
+        file: line.file,
+        size: line.size as number,
+        lastmodificationdate: line.lastmodificationdate as string,
+        hashdate: line.hashdate as string,
+        md5: line.hash as string,
+      }
+      return resp
+    })
+
+    const jsobj = {
+      _declaration: {
+        _attributes: {
+          version: '1.0',
+          encoding: 'utf-8',
+        },
+      },
+      hashlist: {
+        _attributes: {
+          version: '1.0',
+        },
+        creatorinfo: {
+          name: creator.name,
+          username: creator.username,
+          hostname: creator.hostname,
+          tool: creator.tool,
+          startdate: creator.startdate,
+          finishdate: creator.finishdate,
+        },
+        hash: lines,
+      },
+    }
+
+    const xml = js2xml(jsobj, {
+      compact: true,
+      indentCdata: true,
+      spaces: 4,
+      // indentAttributes: true
+    })
+    return xml
   }
 
   public async verify(filepath: string, data: string, opts?: HashVerifyOptions): Promise<string> {
